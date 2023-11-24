69cb11363e4ac5e53648e671dae95d990c90ecf1
Eloho Endurance Anine <114450410+Taikyu@users.noreply.github.com>
2023-11-24 06:37:29 +0100

    Update Card.jsx
    add id prop to card UI

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const Card = ({ className, children }) => {
+const Card = ({id, className, children }) => {
   let classes;
   if (className) {
     classes = className + " sm:rounded-xl border-0 sm:border-1";
   } else {
     classes = "sm:rounded-xl border-0 sm:border-1";
   }
-  return <div className={classes}> {children}</div>;
+  return <div id={id} className={classes}> {children}</div>;
 };
 
 export default Card;
