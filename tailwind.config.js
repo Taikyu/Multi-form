e7acbb795386c58f4d6557e54412bae20f0d5b9a
Eloho Endurance Anine <114450410+Taikyu@users.noreply.github.com>
2023-11-24 05:56:38 +0100

    Update tailwind.config.js
    changed background application method to import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 
+import desktop from "./src/assets/images/bg-sidebar-desktop.svg";
+import mobile from "./src/assets/images/bg-sidebar-mobile.svg";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -12,8 +15,8 @@ export default {
     },
     extend: {
       backgroundImage: {
-        sideBackground: "url('./src/assets/images/bg-sidebar-desktop.svg')",
-        mobileBackground: "url('./src/assets/images/bg-sidebar-mobile.svg')",
+        sideBackground: desktop,
+        mobileBackground: mobile,
       },
       colors: {
         purplishBlue: "hsl(243, 100%, 62%)",
